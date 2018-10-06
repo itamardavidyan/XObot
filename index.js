dce484a6e5a7b27d626795d61b357648da41994f
itamardavidyan <itamardavidyan@gmail.com>
2018-10-06 09:28:26 +0300

    try

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start(ctx => ctx.reply("Welcome!!!"));
+bot.start(ctx => ctx.reply("Welcome!!! :)"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
 bot.on("sticker", ctx => ctx.reply("👍"));
 bot.hears("hi", ctx => ctx.reply("Hey there"));
