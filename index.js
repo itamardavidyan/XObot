ed04ce5ce4bf9b1f0322661c841ef5cd35cf510a
Itamar Davidyan <itamardavidyan@gmail.com>
2018-10-07 14:31:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(ctx => ctx.reply("Welcome!!! :)"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
 bot.on("sticker", ctx => ctx.reply("👍"));
-bot.hears("hi", ctx => ctx.reply("Hey there"));
+// bot.hears("hi", ctx => ctx.reply("Hey there"));
 bot.hears(/buy/i, ctx => ctx.reply("Buy-buy"));
+
+bot.hears("hi", (ctx) => {
+  ctx.reply("Hey there");
+})
 
 bot.startPolling();
