5a8309130fc4295de9b03941f5a853b3de931df3
Itamar Davidyan <itamardavidyan@gmail.com>
2018-10-07 16:36:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ bot.command("play", (ctx) => {
   ctx.reply(ctx.message.from.id);
   ctx.reply(TEMPLATE);
   ctx.reply("who play against you?");
-  players[turn] = "X";
+  players[0] = "X";
 //   players.push({
 //     id: ctx.message.from.id,
 //     sign: "X"
 //   });
-  ctx.reply(players[turn]);
+  ctx.reply(players[0]);
   ctx.reply("here");
 
 //   ctx.reply(players[turn]);
