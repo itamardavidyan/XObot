25ed7faed6936aea292bc88a78c6a5d70a0b0f75
Itamar Davidyan <itamardavidyan@gmail.com>
2018-10-07 14:57:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Telegraf = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 var players = ["ID", "gm"];
+var turn = 0;
 
 const TEMPLATE = "Board Game: \n |     |     |     | \n |     |     |     | \n |     |     |     | ";
 var board = TEMPLATE;
@@ -14,7 +15,9 @@ bot.on("sticker", ctx => ctx.reply("👍"));
 bot.hears(/buy/i, ctx => ctx.reply("Buy-buy"));
 
 bot.command("play", (ctx) => {
-  ctx.reply(players[0]);
+  ctx.reply(players[turn]);
+  if (turn == 0) turn = 1;
+  else turn = 0;
   ctx.reply(ctx.message.from.id);
   ctx.reply(TEMPLATE);
 })
