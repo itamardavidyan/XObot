61f1f5755827ff363d7e886a98759ac6b04a9157
Itamar Davidyan <itamardavidyan@gmail.com>
2018-10-07 14:41:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Telegraf = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const TEMPLATE = "_________ \n |   |   |   | \n _________ \n |   |   |   | \n _________ \n |   |   |   | \n _________ ";
+const TEMPLATE = " |     |     |     | \n |     |     |     | \n |     |     |     | ";
 
 bot.start(ctx => ctx.reply("Welcome!!! :)"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
@@ -10,8 +10,9 @@ bot.on("sticker", ctx => ctx.reply("👍"));
 // bot.hears("hi", ctx => ctx.reply("Hey there"));
 bot.hears(/buy/i, ctx => ctx.reply("Buy-buy"));
 
-bot.hears("hi", (ctx) => {
-  ctx.reply("Hey there2");
+bot.hears("play", (ctx) => {
+  ctx.reply("Hey there3");
+  ctx.reply(ctx);
   ctx.reply(TEMPLATE);
 })
 
