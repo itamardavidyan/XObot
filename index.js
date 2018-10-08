edc9e9c0d68868b75f6df3d34d501138d935e7ad
Itamar Davidyan <itamardavidyan@gmail.com>
2018-10-08 07:48:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,92 +14,92 @@ bot.start(ctx => ctx.reply("Welcome!!! :) \n * send \\strat to play XO \n * your
 bot.help(ctx => ctx.reply("Send me a sticker"));
 bot.on("sticker", ctx => ctx.reply("👍"));
 
-bot.command("me", ctx => {
-  if (players.length == 0) ctx.reply("send \play to start new game");
-  if (players.length >= 2) ctx.reply("2 players already play");
-//   ctx.reply("length: " + players.length);
-  if (players.length != 1) return;
-  const fullName = ctx.message.from.first_name + " " + ctx.message.from.last_name
-  players.push({
-    id: ctx.message.from.id,
-    sign: "O",
-    name: fullName
-  });
-  const msg = players[0].name + " vs " + players[1].name;
-  ctx.reply(msg);
-  ctx.reply("let's the game begin!");
-});
+// bot.command("me", ctx => {
+//   if (players.length == 0) ctx.reply("send \play to start new game");
+//   if (players.length >= 2) ctx.reply("2 players already play");
+// //   ctx.reply("length: " + players.length);
+//   if (players.length != 1) return;
+//   const fullName = ctx.message.from.first_name + " " + ctx.message.from.last_name
+//   players.push({
+//     id: ctx.message.from.id,
+//     sign: "O",
+//     name: fullName
+//   });
+//   const msg = players[0].name + " vs " + players[1].name;
+//   ctx.reply(msg);
+//   ctx.reply("let's the game begin!");
+// });
 
-bot.command("play", (ctx) => {
-//   ctx.reply(ctx.message.from);
-  ctx.reply(STARTTEMPLATE);
-  ctx.reply("who play against you?");
-  const fullName = ctx.message.from.first_name + " " + ctx.message.from.last_name
-  players.push({
-    id: ctx.message.from.id,
-    sign: "X",
-    name: fullName
-  });
-})
+// bot.command("play", (ctx) => {
+// //   ctx.reply(ctx.message.from);
+//   ctx.reply(STARTTEMPLATE);
+//   ctx.reply("who play against you?");
+//   const fullName = ctx.message.from.first_name + " " + ctx.message.from.last_name
+//   players.push({
+//     id: ctx.message.from.id,
+//     sign: "X",
+//     name: fullName
+//   });
+// })
 
-bot.command("end", (ctx) => {
-  ctx.reply("Game Over!");
-  board = [[1,2,3],[4,5,6],[7,8,9]];
-  players = [];
-  turn = 0;
-})
+// bot.command("end", (ctx) => {
+//   ctx.reply("Game Over!");
+//   board = [[1,2,3],[4,5,6],[7,8,9]];
+//   players = [];
+//   turn = 0;
+// })
 
-bot.command("1", (ctx) => play(ctx, "1"))
-bot.command("2", (ctx) => play(ctx, "2"))
-bot.command("3", (ctx) => play(ctx, "3"))
-bot.command("4", (ctx) => play(ctx, "4"))
-bot.command("5", (ctx) => play(ctx, "5"))
-bot.command("6", (ctx) => play(ctx, "6"))
-bot.command("7", (ctx) => play(ctx, "7"))
-bot.command("8", (ctx) => play(ctx, "8"))
-bot.command("9", (ctx) => play(ctx, "9"))
+// bot.command("1", (ctx) => play(ctx, "1"))
+// bot.command("2", (ctx) => play(ctx, "2"))
+// bot.command("3", (ctx) => play(ctx, "3"))
+// bot.command("4", (ctx) => play(ctx, "4"))
+// bot.command("5", (ctx) => play(ctx, "5"))
+// bot.command("6", (ctx) => play(ctx, "6"))
+// bot.command("7", (ctx) => play(ctx, "7"))
+// bot.command("8", (ctx) => play(ctx, "8"))
+// bot.command("9", (ctx) => play(ctx, "9"))
 
-function play(ctx, pos) {
-  var sign = players[turn].sign;
-  // if (ctx.message.from.id == players[turn].id) sign = "X";
-  // ctx.reply("position: " + pos);
-  // ctx.reply("sign: " + sign);
-  var found = false;
+// function play(ctx, pos) {
+//   var sign = players[turn].sign;
+//   // if (ctx.message.from.id == players[turn].id) sign = "X";
+//   // ctx.reply("position: " + pos);
+//   // ctx.reply("sign: " + sign);
+//   var found = false;
   
-  for (let i = 0; i < 3 ; i++ ) {
-    for (let j = 0; j < 3 ; j++ ) { 
-      if (board[i][j] == pos) {
-        board[i][j] = sign;
-        found = true;
-      }
-    }
-  }
-  if (!found) {
-    return;
-    ctx.reply("this position on the board already used.. \n please select another position");
-  }
+//   for (let i = 0; i < 3 ; i++ ) {
+//     for (let j = 0; j < 3 ; j++ ) { 
+//       if (board[i][j] == pos) {
+//         board[i][j] = sign;
+//         found = true;
+//       }
+//     }
+//   }
+//   if (!found) {
+//     return;
+//     ctx.reply("this position on the board already used.. \n please select another position");
+//   }
   
-  ctx.reply(createBoard());
+//   ctx.reply(createBoard());
   
-  if (turn == 0) turn = 1;
-  else turn = 0;
-  ctx.reply(players[turn].name + " it's your turn");
+//   if (turn == 0) turn = 1;
+//   else turn = 0;
+//   ctx.reply(players[turn].name + " it's your turn");
 
-}
+// }
 
-function createBoard() {
-  var print = "";
-  for (let i = 0; i < 3 ; i++ ) {
-    print += "|"
-    for (let j = 0; j < 3 ; j++ ) { 
-      if (board[i][j] == "X" || board[i][j] == "O") print += "  " + board[i][j] + "  |";
-      else print += "     |";
-    }
-    print += "\n ";
-  }
+// function createBoard() {
+//   var print = "";
+//   for (let i = 0; i < 3 ; i++ ) {
+//     print += "|"
+//     for (let j = 0; j < 3 ; j++ ) { 
+//       if (board[i][j] == "X" || board[i][j] == "O") print += "  " + board[i][j] + "  |";
+//       else print += "     |";
+//     }
+//     print += "\n ";
+//   }
   
-  return print;
-}
+//   return print;
+// }
 
 
 bot.catch((err) => {
