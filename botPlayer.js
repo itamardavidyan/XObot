f0d2c9d0b61c1b468a5f7cc5be7ea1faaa250826
itamardavidyan <itamardavidyan@gmail.com>
2018-10-10 03:29:36 +0300

    ai human

--- botPlayer.js
+++ botPlayer.js
@@ -26,82 +26,80 @@ class BotPlayer {
   }
 
   play(board) {
-    return minimax(board, 'O').index;
+    return minimax(board, "O").index;
   }
 }
 
 function emptySquares(board) {
-	var spots = [];
-	var num = 1;
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 3; j++) {
-		  if ( board[i][j] == num.toString() ) {
-			  spots.push(board[i][j]);
-		  }
-		  num++;
-		}
-	}
+  var spots = [];
+  var num = 1;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] == num.toString()) {
+        spots.push(board[i][j]);
+      }
+      num++;
+    }
+  }
 
-	return spots;
-	
+  return spots;
 }
 
 function minimax(newBoard, player) {
-  const huPlayer = 'X';
-  const aiPlayer = 'O';
-	var availSpots = emptySquares(newBoard);
+  const huPlayer = "X";
+  const aiPlayer = "O";
+  var availSpots = emptySquares(newBoard);
 
-	if (getWinner(newBoard, player)) {
-		return {score: -10};
-	} else if (getWinner(newBoard, player)) {
-		return {score: 10};
-	} else if (availSpots.length === 0) {
-		return {score: 0};
-	}
+  if (getWinner(newBoard, huPlayer)) {
+    return { score: -10 };
+  } else if (getWinner(newBoard, aiPlayer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
 
-	var moves = [];
-	for (var i = 0; i < availSpots.length; i++) {
-		var move = {};
-		const spot = parseInt(availSpots[i]);
-		const row = Math.floor((spot - 1) / 3);
-		const col = (spot - 1) % 3;
-		move.index = newBoard[row][col];
-		newBoard[row][col] = player;
+  var moves = [];
+  for (var i = 0; i < availSpots.length; i++) {
+    var move = {};
+    const spot = parseInt(availSpots[i]);
+    const row = Math.floor((spot - 1) / 3);
+    const col = (spot - 1) % 3;
+    move.index = newBoard[row][col];
+    newBoard[row][col] = player;
 
-		if (player == aiPlayer) {
-			var result = minimax(newBoard, huPlayer);
-			move.score = result.score;
-		} else {
-			var result = minimax(newBoard, aiPlayer);
-			move.score = result.score;
-		}
+    if (player == aiPlayer) {
+      var result = minimax(newBoard, huPlayer);
+      move.score = result.score;
+    } else {
+      var result = minimax(newBoard, aiPlayer);
+      move.score = result.score;
+    }
 
-		newBoard[row][col] = move.index;
+    newBoard[row][col] = move.index;
 
-		moves.push(move);
-	}
+    moves.push(move);
+  }
 
-	var bestMove;
-  if(player === aiPlayer) {
+  var bestMove;
+  if (player == aiPlayer) {
     var bestScore = -10000;
-    for(var i = 0; i < moves.length; i++) {
+    for (var i = 0; i < moves.length; i++) {
       if (moves[i].score > bestScore) {
         bestScore = moves[i].score;
         bestMove = i;
       }
     }
-  } 
-  else {
-  	var bestScore = 10000;
-  	for(var i = 0; i < moves.length; i++) {
-  		if (moves[i].score < bestScore) {
-  			bestScore = moves[i].score;
-  			bestMove = i;
-  		}
-  	}
+  } else {
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
   }
 
-	return moves[bestMove];
+  return moves[bestMove];
 }
 
 function allSame(i1, i2, i3, sign, board) {
